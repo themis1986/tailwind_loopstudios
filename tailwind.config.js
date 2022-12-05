655e5e51506ddce83e6c44ab94a51598db388e35
thiakovidis <thiakovidis@agroapps.gr>
2022-12-05 14:26:20 +0200

    Add basic confguration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
-      spacing: {
-        128: "32rem",
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
       },
     },
   },
